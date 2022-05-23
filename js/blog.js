e90d3fcac4c91bb13a088e790e56612f35573bd8
Sandrajasmin <sandra-jasmin@hotmail.com>
2022-05-23 17:46:27 +0200

    changes to nav, colors and margins

--- js/blog.js
+++ js/blog.js
@@ -1,25 +1,13 @@
-const MAX_CONTENT = 10;
-    let contentShown = $(".content").length - $(".content:hidden").length;
-    let displayButton = contentShown >= MAX_CONTENT;
-    if (displayButton) {
-      $("#loadMore").show();
-    } else {
-      $("#loadMore").hide();
-    }
-
-    $(document).ready(function(){
-        $(".content").slice(0, 7).show();
-        $("#loadMore").on("click", function(e){
-          e.preventDefault();
-          $(".content:hidden").slice(0, 7).slideDown();
-          if($(".content:hidden").length == 0) {
-            $("#loadMore").text("No Content").addClass("noContent");
-          }
-        });
-      
-      $('#loadMore').hide();
-      if ($('.content').length > 7) {
-        $('#loadMore').show();
-      }
-        
-      })
+$( document ).ready(function () {
+    $(".moreBox").slice(0, 3).show();
+      if ($(".blogBox:hidden").length != 0) {
+        $("#loadMore").show();
+      }   
+      $("#loadMore").on('click', function (e) {
+        e.preventDefault();
+        $(".moreBox:hidden").slice(0, 6).slideDown();
+        if ($(".moreBox:hidden").length == 0) {
+          $("#loadMore").fadeOut('slow');
+        }
+      });
+    });
--- js/script.js
+++ js/script.js
@@ -1,19 +1,95 @@
+const APIurl = "https://greenskitchen-afd1b0.ingress-daribow.easywp.com/wp-json/wp/v2/posts/"
+
+const lastPost = document.querySelector(".last-post")
+const moreContent = document.querySelector(".slide")
+
+async function getPosts () {
+  try {
+    const response = await fetch(APIurl);
+    const responseJSON = await response.json();
+    const blogInfo = responseJSON;
+    console.log(responseJSON);
+
+    for (blog of blogInfo) {
+    for (image of blog.img){
+      moreContent.innerHTML += `
+                      <img src="/img/smoothie.jpeg" alt="food plater" />
+											<h2>Fresh Smoothie</h2>
+											<p>
+												packed full of healthy fruit and veggies that will help
+												you start the morning off with a good dose of vitamins
+												and minerals.
+											</p>
+      `
+    } 
+    }
+
+      
+  }
+
+  catch (error) {
+    console.log("some error happened", error);
+  }
+}
+
+
+getPosts();
+
+
+// const queryString = document.location.search;
+
+// const params = new URLSearchParams(queryString);
+
+// const APIUrl = ``;
+
+// const blogDetails = document.querySelector(".last-post");
+
+// async function getBlogDetails() {
+//   try {
+//     const response = await fetch(APIUrl);
+//     const singleBlogDetail = await response.json();
+//     console.log('singleBlogDetail:', singleBlogDetail);
+//   } 
+  
+//   catch (error) {
+//     console.log(error);
+//   }
+// }
+// getBlogDetails();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const buttons = document.querySelectorAll("[data-carousel-button]")
 
 buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : - 1
-    const slides = button
-      .closest("[data-carousel]")
-      .querySelector('[data-slides]')
+   button.addEventListener("click", () => {
+     const offset = button.dataset.carouselButton === "next" ? 1 : - 1
+     const slides = button
+       .closest("[data-carousel]")
+       .querySelector('[data-slides]')
 
-    const activeSlide = slides.querySelector("[data-active]")
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0) newIndex = slides.children.length - 1
-    if (newIndex >= slides.children.length) newIndex = 0
+     const activeSlide = slides.querySelector("[data-active]")
+     let newIndex = [...slides.children].indexOf(activeSlide) + offset
+     if (newIndex < 0) newIndex = slides.children.length - 1
+     if (newIndex >= slides.children.length) newIndex = 0
 
-    slides.children[newIndex].dataset.active = true
-    delete activeSlide.dataset.active
-  })
+     slides.children[newIndex].dataset.active = true
+     delete activeSlide.dataset.active
+   }) 
 })
